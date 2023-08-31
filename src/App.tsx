7c6ff3067fad43e119b318a14c3267095cfe14a2
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-08-31 13:53:40 +0200

    Attempted to TypeScript-ify the thing, hope it kinda works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react';
-import { faker } from '@faker-js/faker';
+// Uh, okay, let's start by importing stuff
+import React, { useState } from 'react';
+import { faker } from '@faker-js/faker'; // Wait, is this right?
+import clsx from 'clsx';
 
+// Now, time for some type stuff, I think?
+// I heard TypeScript likes this stuff, whatever it is
+interface TodoItemProps {
+  status: string;
+  label: string;
+  onChecked: any; // I dunno what type this should be, so... any?
+}
+
+// Now for some functions, I guess?
+// What's with those colons and stuff, TypeScript is so picky
 const generateFakeTodoItem = () => ({
   label: faker.hacker.phrase(),
   status: faker.random.arrayElement(['open', 'done', 'archived']),
-  id: faker.string.uuid(),
+  id: faker.random.uuid(),
 });
 
+// This function takes a number, right? So it's like... size: number?
 const generateNTodo = (size) => {
   return Array.from(Array(size).keys()).map(generateFakeTodoItem);
 };
 
+// Ugh, more type stuff
 const initialList = [
   {
     label: 'This is my first todo item',
     status: 'open',
-    id: faker.string.uuid(),
+    id: faker.random.uuid(),
   },
   {
     label: 'This is some done todo',
     status: 'done',
-    id: faker.string.uuid(),
+    id: faker.random.uuid(),
   },
   {
     label: 'This is a really old todo',
     status: 'archived',
-    id: faker.string.uuid(),
+    id: faker.random.uuid(),
   },
   ...generateNTodo(10),
 ];
 
+// Okay, now for the TodoItem thing
+// What was that clsx thing again?
 const TodoItem = ({ status, label, onChecked }) => {
   return (
     <div
@@ -53,10 +69,11 @@ const TodoItem = ({ status, label, onChecked }) => {
   );
 };
 
-
+// Wait, how do you make a function component again?
 function App() {
   const [todoList, setTodoList] = useState(initialList);
 
+  // Uh, this updater thing... is it even gonna work?
   const updater = (id, newStatus) => {
     setTodoList((oldList) =>
       oldList.map((it) => {
@@ -67,10 +84,11 @@ function App() {
           ...it,
           status: newStatus,
         };
-      }),
+      })
     );
   };
 
+  // Okay, wrapping up I guess
   return (
     <div className="bg-white shadow rounded-lg py-8">
       <div className="divide-gray-300 divide-y">
@@ -87,4 +105,5 @@ function App() {
   );
 }
 
+// Exporting stuff, I think
 export default App;
